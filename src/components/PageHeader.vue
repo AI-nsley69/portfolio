ec88a07bcb76bd26ded7adea7fe061f89e528cec
Benjamin Aronsson <benjamin.aronsson@flowbird.group>
2024-07-22 16:04:23 +0200

    Create a better gradient for page headers

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,6 +1,6 @@
 <template>
-    <h2 class="text-2xl w-full text-left mb-4">{{ title || 'Placeholder' }}</h2>
-    <div class="w-1/5 fading-bottom-border mb-4"></div>
+    <h2 class="text-2xl w-full text-left mb-4">{{ title }}</h2>
+    <div class="w-1/5 bg-gradient-to-r from-white via-neutral-500 to-transparent h-0.5 mb-4"></div>
 </template>
 
 <script setup lang="ts">
@@ -10,10 +10,4 @@ defineProps({
 </script>
 
 <style scoped>
-.fading-bottom-border {
-  background:
-    linear-gradient(var(--primary), var(--primary)) padding-box,
-    linear-gradient(to right, rgba(255, 255, 255, 255) 0%, rgba(255, 255, 255, 255) 70%, rgba(255, 255, 255, 0) 100%) border-box;
-  border-bottom: 2px solid transparent;
-}
 </style>
