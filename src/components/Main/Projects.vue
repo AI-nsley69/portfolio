71fbf5a81f35e30b4e61971ea78ef183b06f0981
Benjamin Aronsson <benjamin.aronsson@flowbird.group>
2024-07-22 15:27:57 +0200

    Better project cards

--- src/components/Main/Projects.vue
+++ src/components/Main/Projects.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="min-h-screen h-max flex flex-col px-4">
         <PageHeader title="Projects" />
-        <ul class="flex flex-row flex-wrap gap-4 w-full justify-center">
+        <ul class="grid grid-cols-1 mt-10 text-center sm:mt-16 sm:grid-cols-2 sm:gap-x-12 gap-y-12 md:grid-cols-3 md:gap-0 xl:mt-24">
             <ProjectItem v-for="project in projects" :project="project" :key="project.url" />
         </ul>
     </section>
--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -1,33 +1,15 @@
 <template>
   <li
-    class="lg:w-2/5 sm:4/5 relative overflow-hidden rounded-lg border bg-transparent select-none hover:shadow hover:shadow-teal-200 p-2"
+    class="md:p-8 lg:p-14 flex flex-col justify-center items-center rounded-md border-2 border-rose-800 m-2"
   >
-    <div class="flex h-[180px] flex-col justify-between rounded-md p-6">
-      <img :src="`${icon}`" class="h-12 w-12 fill-current rounded-md" />
-      <div class="space-y-2">
-        <h3 class="font-bold text-4xl">{{ title }}</h3>
-        <p class="text-md">{{ description }}</p>
-      </div>
-    </div>
-  </li>
-  <!-- <div class="bg-transparent lg:w-2/5 sm:w-5/6 lg:h-48 sm:h-64 flex flex-col justify-center items-center">
     <div
-      class="bg-gradient-to-r from-pink-500 to-rose-600 rounded-lg shadow-lg p-4 w-full h-full"
+      class="w-14 h-14 rounded-full bg-transparent flex justify-center items-center"
     >
-      <h1 class="text-2xl font-bold text-white mb-4">
-        {{ title }}
-      </h1>
-      <p class="text-md text-white mb-8">
-        {{ description }}
-      </p>
-      <a
-        :href="`${url}`"
-        class="bg-white hover:bg-gray-200 text-purple-600 font-bold py-2 px-4 rounded"
-      >
-        Source
-      </a>
+      <i class="fa-brands fa-github text-4xl text-neutral-100"></i>
     </div>
-  </div> -->
+    <h3 class="mt-12 text-xl font-bold text-neutral-100">{{ title }}</h3>
+    <p class="mt-5 text-base text-gray-300">{{ description }}</p>
+  </li>
 </template>
 
 <script setup lang="ts">
