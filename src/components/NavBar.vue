665c6b1639fe1f5c743748327a9908f71aea2329
Benjamin Aronsson <benjamin.aronsson@flowbird.group>
2024-07-22 16:07:40 +0200

    Add z-index to make sure navbar is always on top

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,20 +1,29 @@
 <template>
   <div
-    class="absolute right-2 top-1/2 bottom-1/2 m-auto w-12 h-fit bg-neutral-700 px-2 rounded-xl"
+    class="absolute right-2 top-1/2 bottom-1/2 m-auto w-12 h-fit bg-neutral-700 px-2 rounded-xl z-50"
   >
     <div
       id="content"
       class="flex flex-col items-center justify-center py-2 lg:py-4 gap-4"
     >
-    <span class="w-4/5 text-white text-xl cursor-pointer hover:scale-[1.02] active:scale-[0.98] hover:text-red-400" @click="triggerScroll('#work-experience')">
+      <span
+        class="w-4/5 text-white text-xl cursor-pointer hover:scale-[1.02] active:scale-[0.98] hover:text-red-400"
+        @click="triggerScroll('#work-experience')"
+      >
         <i class="fa-solid fa-person-digging"></i>
-    </span>
-    <span class="w-4/5 text-white text-xl cursor-pointer hover:scale-[1.02] active:scale-[0.98] hover:text-red-400" @click="triggerScroll('#projects')">
+      </span>
+      <span
+        class="w-4/5 text-white text-xl cursor-pointer hover:scale-[1.02] active:scale-[0.98] hover:text-red-400"
+        @click="triggerScroll('#projects')"
+      >
         <i class="fa-solid fa-briefcase"></i>
-    </span>
-    <span class="w-4/5 text-white text-xl cursor-pointer hover:scale-[1.02] active:scale-[0.98] hover:text-red-400" @click="triggerScroll('#skills')">
+      </span>
+      <span
+        class="w-4/5 text-white text-xl cursor-pointer hover:scale-[1.02] active:scale-[0.98] hover:text-red-400"
+        @click="triggerScroll('#skills')"
+      >
         <i class="fa-solid fa-gears"></i>
-    </span>
+      </span>
     </div>
   </div>
 </template>
