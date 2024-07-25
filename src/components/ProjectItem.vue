1a83d27b45c357ab683d4d283a45a0008e78da7f
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2024-07-25 19:03:29 +0200

    Slightly improve projectitem

--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -13,7 +13,7 @@
 
       <img
         :src="icon"
-        class="w-full h-full object-cover col-span-2"
+        class="w-full h-full object-center object-cover col-span-2"
         loading="lazy"
         :alt="`${title} project image`"
       />
@@ -22,15 +22,18 @@
         <h3 class="mt-12 text-3xl font-bold text-neutral-100">{{ title }}</h3>
         <p class="mt-5 text-base text-neutral-400">{{ description }}</p>
         <div class="relative inline-flex group mt-4">
-            <div
-                class="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] blur-sm group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt rounded-lg">
-            </div>
-            <a :href="url"
-                class="relative gap-2 inline-flex items-center justify-center p-2 text-lg font-bold text-white hover:text-white transition-all duration-200 bg-black font-pj focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 rounded-lg"
-                role="button">
-                <font-awesome-icon :icon="['fa-brands', 'github']" class="h-5" />
-                <span>Source</span>
-            </a>
+          <div
+            class="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] blur-sm group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt rounded-lg"
+          ></div>
+          <a
+            :href="url"
+            target="_blank"
+            class="relative gap-2 inline-flex items-center justify-center p-2 text-lg font-bold text-white hover:text-white transition-all duration-200 bg-black font-pj focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 rounded-lg"
+            role="button"
+          >
+            <font-awesome-icon :icon="['fa-brands', 'github']" class="h-5" />
+            <span>Source</span>
+          </a>
         </div>
       </div>
     </div>
