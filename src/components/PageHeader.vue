b93265519cf9d808135748ec7c092e2e174e45ba
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2024-07-25 19:03:09 +0200

    Minor improvements to page header

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,13 +1,16 @@
 <template>
-    <h2 class="text-2xl w-full text-left mb-4">{{ title }}</h2>
-    <div class="w-1/5 bg-gradient-to-r from-white via-neutral-500 to-transparent h-0.5 mb-4"></div>
+  <h2 class="md:text-2xl lg:text-4xl w-full text-left mb-4 font-bold">
+    {{ title }}
+  </h2>
+  <div
+    class="w-1/5 bg-gradient-to-r from-white via-neutral-500 to-transparent h-0.5 mb-4"
+  ></div>
 </template>
 
 <script setup lang="ts">
 defineProps({
-    title: String,
+  title: String,
 });
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
