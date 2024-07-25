bd42749dc8b7129b82edc26b19596951ddc51bf2
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2024-07-25 22:01:59 +0200

    Remove projects as a fetched file and bake it into code instead

--- src/components/Main/Projects.vue
+++ src/components/Main/Projects.vue
@@ -14,7 +14,7 @@
 </template>
 
 <script setup lang="ts">
-import { defineComponent, onMounted, ref, type Ref } from "vue";
+import { defineComponent, ref, type Ref } from "vue";
 import ProjectItem from "../ProjectItem.vue";
 import PageHeader from "../PageHeader.vue";
 
@@ -32,13 +32,45 @@ interface Project {
   icon: string;
 }
 
-let projects: Ref<Project[]> = ref([]);
-onMounted(async () => {
-  const res = await fetch("/data/projects.json");
-  if (!res.ok) return;
-
-  projects.value = await res.json();
-});
+let projects: Ref<Project[]> = ref([
+  {
+    title: "Nyasharp",
+    description: "Esoteric language written in C#",
+    url: "https://github.com/AI-nsley69/nyasharp",
+    icon: "/projects/nyasharp.jpg",
+  },
+  {
+    title: "I Suck",
+    description: "Novice Minecraft tweaks",
+    url: "https://github.com/AI-nsley69/i-suck",
+    icon: "/projects/i-suck.jpg",
+  },
+  {
+    title: "imbed",
+    description: "Embed messages into images with encryption",
+    url: "https://github.com/AI-nsley69/imbed",
+    icon: "/projects/imbed.jpg",
+  },
+  {
+    title: "Khaos Utils",
+    description:
+      "A special-purpose Discord Bot written in Node.js for a Minecraft community",
+    url: "https://github.com/KhaosMC/khaos-utils",
+    icon: "/projects/khaos-utils.jpg",
+  },
+  {
+    title: "ZigBrot",
+    description: "A simple mandelbrot renderer with basic multi-threading",
+    url: "https://github.com/AI-nsley69/ZigBrot",
+    icon: "/projects/mandelbrot.jpg",
+  },
+  {
+    title: "MonoPew",
+    description: "Simple Space Invaders-like game written in MonoGame",
+    url: "https://github.com/AI-nsley69/MonoPew",
+    icon: "/projects/monopew.jpg",
+  },
+]);
 </script>
 
 <style scoped>
