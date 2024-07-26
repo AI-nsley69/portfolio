fa02f6d8758c440918f332a0d868b24b05f9ac15
Benjamin Aronsson <benjamin.aronsson@flowbird.group>
2024-07-26 14:11:48 +0200

    Use site names instead of asset type

--- src/components/Layout/Footer.vue
+++ src/components/Layout/Footer.vue
@@ -5,8 +5,8 @@
     <div class="col-span-1 w-full flex flex-col justify-start items-center py-2 gap-2">
         <span class="uppercase tracking-wider font-bold">Assets</span>
         <div class="w-3/5 h-[2px] bg-gradient-to-r from-transparent via-cod-gray-600 to-transparent mb-4"></div>
-        <a href="https://icons8.com" target="_blank" class="text-neutral-300 hover:text-cardinal-400">Icons</a>
-        <a href="https://unsplash.com" target="_blank" class="text-neutral-300 hover:text-cardinal-400">Images</a>
+        <a href="https://unsplash.com" target="_blank" class="text-neutral-300 hover:text-cardinal-400">Unsplash</a>
+        <a href="https://icons8.com" target="_blank" class="text-neutral-300 hover:text-cardinal-400">Icons8</a>
     </div>
     <div class="col-span-3 border-x-2 border-cod-gray-700 w-full h-full"></div>
     <div class="col-span-1 w-full h-full flex flex-col justify-start items-center py-2 gap-2">
