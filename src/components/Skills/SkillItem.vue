aef44eb1a17327d0ff206feb1851d15176974342
Benjamin Aronsson <benjamin.aronsson@flowbird.group>
2024-07-26 08:32:44 +0200

    Make skill cards smaller

--- src/components/Skills/SkillItem.vue
+++ src/components/Skills/SkillItem.vue
@@ -1,19 +1,19 @@
 <template>
   <li class="px-3 py-3">
     <div
-      class="flex flex-col max-w-sm rounded-lg overflow-hidden shadow-lg bg-gradient-to-tr from-fuchsia-700 to-violet-600 h-full"
+      class="flex flex-col max-w-sm rounded-lg overflow-hidden shadow-lg bg-gradient-to-tr from-fuchsia-700 to-violet-600 w-72 h-64"
     >
       <img
-        class="w-1/2 max-h-1/4 mx-auto"
+        class="w-24 mx-auto"
         :src="icon"
         :alt="`${name} skill icon`"
         loading="lazy"
       />
-      <div class="px-6 py-4">
+      <div class="py-2">
         <div class="font-bold text-xl mb-2">{{ name }}</div>
-        <p class="text-gray-300 text-base">
+        <!-- <p class="text-gray-300 text-base">
           {{ description }}
-        </p>
+        </p> -->
       </div>
       <div class="flex-1 min-h-1 w-full"></div>
       <div
@@ -21,7 +21,7 @@
       >
         <span
           v-for="tag in tags"
-          class="inline-block bg-white rounded-full text-sm font-semibold text-violet-600 mr-2 p-2"
+          class="inline-block bg-white rounded-full text-sm font-semibold text-violet-600 p-2"
           >#{{ tag }}</span
         >
       </div>
