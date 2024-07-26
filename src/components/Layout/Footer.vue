a0407c2e32759166fe8fb87b83be4bc8a589d3d4
Benjamin Aronsson <benjamin.aronsson@flowbird.group>
2024-07-26 14:08:08 +0200

    Add a basic footer

--- src/components/Layout/Footer.vue
+++ src/components/Layout/Footer.vue
@@ -1,5 +1,19 @@
 <template>
   <footer
-    class="flex flex-col items-center justify-center gap-4 bg-cod-gray-950 min-h-64 w-full"
-  ></footer>
+    class="grid grid-cols-5 gap-4 bg-cod-gray-950 min-h-64 w-full py-4"
+  >
+    <div class="col-span-1 w-full flex flex-col justify-start items-center py-2 gap-2">
+        <span class="uppercase tracking-wider">Assets sources</span>
+        <div class="w-3/5 h-[2px] bg-gradient-to-r from-transparent via-cod-gray-600 to-transparent mb-4"></div>
+        <a href="https://icons8.com" target="_blank" class="text-white hover:text-cardinal-400">Icons</a>
+        <a href="https://unsplash.com" target="_blank" class="text-white hover:text-cardinal-400">Images</a>
+    </div>
+    <div class="col-span-3 border-x-2 border-cod-gray-700 w-full h-full"></div>
+    <div class="col-span-1 w-full h-full flex flex-col justify-start items-center py-2 gap-2">
+        <span class="uppercase tracking-wider">Built with</span>
+        <div class="w-3/5 h-[2px] bg-gradient-to-r from-transparent via-cod-gray-600 to-transparent mb-4"></div>
+        <a href="https://vuejs.org/" target="_blank" class="text-white hover:text-cardinal-400">Vue.js</a>
+        <a href="https://tailwindcss.com/" target="_blank" class="text-white hover:text-cardinal-400">Tailwind CSS</a>
+    </div>
+  </footer>
 </template>
