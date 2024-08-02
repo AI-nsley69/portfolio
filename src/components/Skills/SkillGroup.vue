2ea771b57a672cc9157b6a2f170c48a87967a8c6
Benjamin Aronsson <benjamin.aronsson@flowbird.group>
2024-08-02 16:02:29 +0200

    Add blur to skills

--- src/components/Skills/SkillGroup.vue
+++ src/components/Skills/SkillGroup.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="sm:w-4/5 lg:w-2/3 mx-auto h-fit bg-white/30 p-4 rounded-box">
-    <h2 class="text-3xl font-bold text-neutral-300 drop-shadow-lg mb-4">
+  <div class="sm:w-4/5 lg:w-2/3 mx-auto h-fit bg-white/20 p-4 rounded-box backdrop-blur-md">
+    <h2 class="text-3xl font-bold text-neutral-100 drop-shadow-lg mb-4">
       {{ name }}
     </h2>
     <ul class="flex flex-row flex-wrap justify-center gap-8 mt-10 text-center">
@@ -10,7 +10,7 @@
 </template>
 
 <script setup lang="ts">
-import { Skill } from "../../helpers/skills";
+import type { Skill } from "../../helpers/skills";
 import SkillItem from "./SkillItem.vue";
 
 interface IProp {
