93609a4d7f3625ab56be618b72411bebf75b7420
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2024-09-10 23:02:26 +0200

    Center skill group texts

--- src/components/Skills/SkillGroup.vue
+++ src/components/Skills/SkillGroup.vue
@@ -1,6 +1,10 @@
 <template>
-  <div class="sm:w-4/5 lg:w-2/3 mx-auto h-fit bg-white/20 p-4 rounded-box backdrop-blur-md">
-    <h2 class="text-3xl font-bold text-neutral-100 drop-shadow-lg mb-4">
+  <div
+    class="sm:w-4/5 lg:w-2/3 mx-auto h-fit bg-white/20 p-4 rounded-box backdrop-blur-md"
+  >
+    <h2
+      class="text-3xl font-bold text-neutral-100 drop-shadow-lg mb-4 text-center"
+    >
       {{ name }}
     </h2>
     <ul class="flex flex-row flex-wrap justify-center gap-8 mt-10 text-center">
